4fa0c41fadf75a5483ba45034803d4f59f1061d3
niaak <57080476+YuhoLee@users.noreply.github.com>
2024-09-29 16:05:09 +0900

    chore: init project

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UberEats"
+include(":app")
+include(":core:designsystem")
+include(":core:data")
+include(":core:domain")
+include(":presentation")
