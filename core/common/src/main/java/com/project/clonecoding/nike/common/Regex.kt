5d7480755e2855870e25f75ea2bc52fb81a7359d
niaak <57080476+YuhoLee@users.noreply.github.com>
2024-10-09 04:50:46 +0900

    Feat: 카드사 카드번호 관련 Regex 추가

--- core/common/src/main/java/com/project/clonecoding/nike/common/Regex.kt
+++ core/common/src/main/java/com/project/clonecoding/nike/common/Regex.kt
@@ -1,9 +1,10 @@
 package com.project.clonecoding.nike.common
 
 import android.util.Patterns
+import com.project.clonecoding.nike.common.data.CardType
 import java.util.regex.Pattern
 
-object Regex {
+object AccountRegex {
     const val AUTH_CODE_MAX_LENGTH = 6
     const val PW_MIN_LENGTH = 8
     const val PW_MAX_LENGTH = 16
@@ -33,4 +34,53 @@ object Regex {
 
     fun checkNicknameProgressRegex(nickname: String):Boolean =
         Pattern.matches(NICKNAME_PROGRESS_REGEX, nickname)
+}
+
+object CardRegex {
+
+    private const val MASTER_CARD_REGEX = "^((22(?!20)[2-9][0-9]|2[3-6][0-9]{2}|27[0-1][0-9]|2720))$"
+    private const val MAESTRO_CARD_REGEX = "^(3[47][0-9]{2})$"
+    private const val VISA_CARD_REGEX = "^(35(2[8-9]|[3-8][0-9]))$"
+    private const val JCB_CARD_REGEX = "^(36[0-9]{2})$"
+    private const val AMEX_CARD_REGEX = "^(4[0-9]{3})$"
+    private const val DINERSCLUB_CARD_REGEX = "^(5[1-5][0-9]{2})$"
+    private const val UNIONPAY_CARD_REGEX = "^(50[0-9]{2}|5[6-9][0-9]{2})$"
+    private const val DISCOVER_CARD_REGEX = "^(60(?!60)[0-9]{2}|61[0-9]{2}|64[0-9]{2})$"
+
+
+    /**
+     * 카드 번호에 따라 어떤 카드 종류인지 반환한다
+     * @param cardNumber 카드 번호
+     * @return 카드 종류 반환, nullable
+     */
+    fun getMatchedCardType(cardNumber: String): CardType?{
+        if(cardNumber.length < 4) return null
+        val cn = if(cardNumber.length > 4) cardNumber.substring(0 until 4) else cardNumber
+
+        return when{
+            Pattern.matches(MASTER_CARD_REGEX, cn) -> CardType.MasterCard
+            Pattern.matches(MAESTRO_CARD_REGEX, cn) -> CardType.MaestroCard
+            Pattern.matches(VISA_CARD_REGEX, cn) -> CardType.VisaCard
+            Pattern.matches(JCB_CARD_REGEX, cn) -> CardType.JcbCard
+            Pattern.matches(AMEX_CARD_REGEX, cn) -> CardType.AmexCard
+            Pattern.matches(DINERSCLUB_CARD_REGEX, cn) -> CardType.DinersclubCard
+            Pattern.matches(UNIONPAY_CARD_REGEX, cn) -> CardType.UnionpayCard
+            Pattern.matches(DISCOVER_CARD_REGEX, cn) -> CardType.DiscoverCard
+            else -> null
+        }
+    }
+
+    /**
+     * 카트 종류에 따른 자리수를 반환한다
+     * @param cardType 카드 종류, nullable
+     * @return 자리수 반환, nullable
+     */
+    fun getCardTypeDigit(cardType: CardType?): Int?{
+        return when(cardType){
+            null -> null
+            CardType.AmexCard -> 15
+            CardType.DinersclubCard -> 14
+            else -> 16
+        }
+    }
 }
